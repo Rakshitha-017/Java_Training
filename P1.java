e70f82681f78511d7270e24e0c408b3bad84d43d
Rakshitha-017 <rakshitha.j017@gmail.com>
2026-02-03 10:46:06 +0530

    Programs

---
+++ P1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class P1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the temperature in celsius:");
+        int n = sc.nextInt();
+        if (n<20){
+            System.out.println("Cool");
+        }
+        else if(n > 30 && n<50){
+            System.out.println("Hot");
+        }
+        else {
+            System.out.println("More hot");
+        }
+    }
+}
